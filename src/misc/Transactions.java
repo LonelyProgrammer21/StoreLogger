2a0edbf5470fd34a7d0fd9e1e46f343e5749c280
LonelyProgrammer <jeric_tayson@protonmail.com>
2022-02-24 13:39:45 +0800

    Added Transaction Model

---
+++ src/misc/Transactions.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2022 lonelyprogrammer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package misc;
+
+/**
+ *
+ * @author lonelyprogrammer
+ */
+public class Transactions {
+    
+    private String month;
+    private int date;
+    private int year;
+    private String productName;
+    private double price;
+    private int quantity;
+
+    public Transactions(String month, int date, int year, String productName, double price, int quantity) {
+        this.month = month;
+        this.date = date;
+        this.year = year;
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    
+    public Transactions(){}
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+    
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+    
+}
