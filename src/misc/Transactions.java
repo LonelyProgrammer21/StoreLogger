5f04c3178813d557c4a05c3e3f73571ff0e2c90f
LonelyProgrammer21 <jeric_tayson@protonmail.com>
2022-02-26 14:17:30 +0800

    Added total balance variable for transaction class.

--- src/misc/Transactions.java
+++ src/misc/Transactions.java
@@ -1,19 +1,5 @@
-/*
- * Copyright (C) 2022 lonelyprogrammer
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+
+
 package misc;
 
 /**
@@ -28,6 +14,7 @@ public class Transactions {
     private String productName;
     private double price;
     private int quantity;
+    private double totalBalance;
     private String time;
 
     public Transactions(String month, int date, int year, String time, String productName, double price, int quantity) {
@@ -38,6 +25,14 @@ public class Transactions {
         this.price = price;
         this.quantity = quantity;
         this.time = time;
+    }
+    
+    public double getTotalBalance() {
+        return totalBalance;
+    }
+
+    public void setTotalBalance(double totalBalance) {
+        this.totalBalance = totalBalance;
     }
 
     public String getTime() {
